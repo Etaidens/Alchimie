172825484ce2da22370939afb81eb0d985cb4281
Etaidens <50491937+Etaidens@users.noreply.github.com>
2021-04-04 14:13:36 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function alchimisation(valeur) {
+    var valeur = document.querySelector('#valeur').value;
+    var niveau = document.querySelector('#niveau').value;
+    var de = document.querySelector('#de').value;
+    var qualite = document.querySelector('#qualite').value;
+    var equipement = document.querySelector('#equipement').value;
+    divise = Math.round(de / 30)
+
+    return (valeur * 2 + qualite * 2) * ( (niveau + 1)  * divise) * equipement;
+}
